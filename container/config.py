60f99a494f0930a622e0a9de998bd2b2a52ef8a9
Russ Garrett <russ@garrett.co.uk>
2021-05-16 11:44:43 +0100

    Index page

--- container/config.py
+++ container/config.py
@@ -1,10 +1,28 @@
-# WSGI module for use with Apache mod_wsgi or gunicorn
-
-# # uncomment the following lines for logging
-# # create a log.ini with `mapproxy-util create -t log-ini`
-# from logging.config import fileConfig
-# import os.path
-# fileConfig(r'/app/log.ini', {'here': os.path.dirname(__file__)})
-
 from mapproxy.wsgiapp import make_wsgi_app
-application = make_wsgi_app(r'/app/mapproxy.yaml')
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from flask import render_template
+from flask import Flask
+
+flask_app = Flask(__name__)
+
+
+@flask_app.route("/")
+def index():
+    return render_template('index.html')
+
+
+class AddPrefixMiddleware(object):
+    def __init__(self, app, prefix):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, e, h):
+        e["PATH_INFO"] = self.prefix + e["PATH_INFO"]
+        return self.app(e, h)
+
+
+mapproxy_app = make_wsgi_app(r"/app/mapproxy.yaml")
+
+application = DispatcherMiddleware(
+    flask_app, {"/tiles": AddPrefixMiddleware(mapproxy_app, "/tiles")}
+)
